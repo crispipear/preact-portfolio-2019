190fc8105e6539ddccbc824251e7fdcbb4a8e58a
crispipear <lisuy97@gmail.com>
2020-06-23 14:18:15 -0700

    updated 2020

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-import { h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
-
 const links = {
 	LinkedIn: 'https://www.linkedin.com/in/suyingli/',
 	Dribbble: 'https://dribbble.com/crispipear',
 	CodePen: 'https://codepen.io/crispipear/',
-	Github: 'https://github.com/crispipear'
+	Github: 'https://github.com/crispipear',
+	Contact: 'mailto:lisuy97@gmail.com'
 }
 
 export default function App(){
 	return(
 		<div id="app">
-			<h1>&#127959;</h1>
-			<p>Hello! This is Su, thanks for visiting.</p>
-			<p>My portfolio is currently under construction, please come back soon. &#128513;</p>
+			<span>&#127959;</span>
+			<h1>Hello! This is Su, thanks for visiting.</h1>
+			<p>My portfolio site is currently under construction, please come back soon. &#128513;</p>
 			<p>
 				{
 					Object.keys(links).map((linkName, key) =>
--- src/lib/case-studies/index.js
+++
@@ -1,8 +0,0 @@
-import clinic from './clinic';
-import journi from './journi';
-import smartcabin from './smartcabin';
-import unarchived from './unarchived';
-
-export default {
-    unarchived, journi, smartcabin, clinic,
-}
